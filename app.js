af718736209350b91fa37f1741b744d9b70b87b1
lfernando2091 <lfernando2091@gmail.com>
2018-12-16 22:43:45 -0600

    Creación de script SQL

--- app.js
+++ app.js
@@ -1,13 +1,81 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const debug = require('debug')('app --> ');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const fs = require('fs');
+/*Call Mappers Controller*/
+const mappers = require('./lib/mappers.js');
+mappers.loadMapper('login');
+mappers.setNameSpace('login');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/**
+ * Express bcrypt for sometext
+ */
+const bcrypt = require('bcrypt-nodejs');
+/**
+ * Express Passport Controller
+ */
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+/**
+ * Express UUID creator
+ */
+const uuid = require('uuid/v4');
+/**
+ * Express Session Controller
+ */
+const session = require('express-session');
+/**
+ * Express Grant Middleware Controller
+ */
+const grant = require('grant-express');
+/**
+ * Express MySql Middleware Session saver
+ */
+const MySQLStore = require('express-mysql-session')(session);
+/**
+ * Express MySql
+ */
+const MySql = require('mysql');
+/**
+ * Express MySql Middleware
+ */
+const MySqlConnection = require('express-myconnection');
+
+const expressValidator = require('express-validator');
+
+/**
+ * This module shows flash messages
+ * generally used to show success or error messages
+ * 
+ * Flash messages are stored in session
+ * So, we also have to install and use 
+ * cookie-parser & session modules
+ */ 
+const flash = require('express-flash');
+
+/*Config Options For Grant*/
+const config = require('./config-grant.json');
+
+/*Config Options For MySql Connection*/
+const options = require('./config-db-options.json');
+
+/*"ssl": {},*/
+/*options.ssl =
+{
+    cat: fs.readFileSync(__dirname + '/certs/old-server-ca.pem'),
+    key: fs.readFileSync(__dirname + '/certs/client-key.pem'),
+    cert: fs.readFileSync(__dirname + '/certs/client-cert.pem')
+}*/
+
+const sessionStore = new MySQLStore(options);
 
 var app = express();
+
+const secretKey = 'secret-key-api-change-in-production';
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +87,98 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(flash());
+
+app.use(expressValidator());
+
+/*Set middleware for Mysql Connection*/
+app.use(MySqlConnection(MySql, options, 'single'));
+
+// REQUIRED: any session store - see /examples/express-session-stores
+app.use(
+    session(
+        {
+            store: sessionStore,
+            key: 'mitienda',
+            secret: secretKey, 
+            genid: (req) => {
+              return uuid() // use UUIDs for session IDs
+            },
+            saveUninitialized: true, 
+            resave: true
+        }
+    ));
+// mount grant
+app.use(grant(config));
+/**
+ * Express Passport Settings
+ */ 
+var users = new Array();
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'username',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+    // Load hash from your password DB.
+    //bcrypt.compare("bacon", hash, function(err, res) {
+        // res == true
+    //});
+    // SQL Parameters
+    var param = {
+        usuario : username
+    } 
+    // create the connection to database
+    MySql.createConnection(options).query(mappers.onQuery('authenticate',param), function (error, results, fields) {
+        if (error) return done(err); 
+        users = results;      
+        if (results.length==0)
+          return done(null, false, { message: 'Usuario no registrado.' }); 
+        if(!bcrypt.compareSync(password, results[0].Token)) 
+          return done(null, false, { message: 'Incorrect username o password.' }); 
+        return done(null, results[0]);             
+    });    
+  }
+));
+
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+  done(null, user.Id);
+});
+
+
+passport.deserializeUser(function(id, done) {
+  // SQL Parameters
+  var user;
+  if(users.length>0)
+    user = users[0].Id === id ? users[0] : false; 
+  else 
+    user = false;
+  done(null, user);
+  //User.findById(id, function(err, user) {
+  //  done(err, user);
+  //});
+
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+/*Call to router Controller*/
+require('./lib/routes.js')(app, passport);
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain')
+  res.write('you posted:\n')
+  res.end(JSON.stringify(req.body, null, 2))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ sql/incidencias.sql
@@ -0,0 +1,180 @@
+DROP DATABASE IF EXISTS incidencias;
+
+CREATE DATABASE incidencias;
+
+USE incidencias;
+
+CREATE TABLE usuario(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	usuario TEXT NOT NULL,
+	tipo ENUM('admin', 'usuario') DEFAULT 'usuario',
+	token TEXT NOT NULL,
+	estatus ENUM('activo', 'inactivo') DEFAULT 'activo',
+	fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	fecha_ultima_mod TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categoria_incidencia(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	nombre TEXT NOT NULL,
+	estatus ENUM('activo', 'inactivo') DEFAULT 'activo'
+);
+
+CREATE TABLE periodo(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	nombre TEXT NOT NULL,
+	clave TEXT NOT NULL,
+	estatus ENUM('activo', 'inactivo') DEFAULT 'activo'
+);
+
+CREATE TABLE criterio(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	nombre TEXT NOT NULL,
+	clave TEXT NOT NULL,
+	estatus ENUM('activo', 'inactivo') DEFAULT 'activo'
+);
+
+CREATE TABLE programa_educativo(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	nombre TEXT NOT NULL,
+	clave TEXT NOT NULL,
+	estatus ENUM('activo', 'inactivo') DEFAULT 'activo'
+);
+
+CREATE TABLE docente(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	nombre TEXT NOT NULL,
+	apellido_paterno TEXT NOT NULL,
+	apellido_materno TEXT NOT NULL,
+	correo_electronico TEXT NOT NULL,
+	fecha_nacimiento DATE NOT NULL,
+	cedula_profesional TEXT NOT NULL,
+	numero_telefonico TEXT NOT NULL,
+	fotografia TEXT,
+	tipo_horario ENUM('parcial', 'completo') DEFAULT 'parcial',
+	estatus ENUM('activo', 'inactivo') DEFAULT 'activo',
+	fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	fecha_ultima_mod TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE docente_bajas(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	id_docente INT, INDEX(id_docente), FOREIGN KEY (id_docente) REFERENCES docente(pkey),
+	comentario TEXT NOT NULL,
+	tipo_baja ENUM('temporal', 'definitivo') DEFAULT 'temporal',
+	fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	fecha_ultima_mod TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE docente_calificacion(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	id_docente INT, INDEX(id_docente), FOREIGN KEY (id_docente) REFERENCES docente(pkey),
+	id_periodo INT, INDEX(id_periodo), FOREIGN KEY (id_periodo) REFERENCES periodo(pkey),
+	fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	fecha_ultima_mod TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE docente_calificacion_criterio(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	id_docente_calificacion INT, INDEX(id_docente_calificacion), FOREIGN KEY (id_docente_calificacion) REFERENCES docente_calificacion(pkey),
+	id_criterio INT, INDEX(id_criterio), FOREIGN KEY (id_criterio) REFERENCES criterio(pkey),
+	calificacion DECIMAL(3,2) NOT NULL DEFAULT '0.00',
+	fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	fecha_ultima_mod TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE docente_incidencia(
+	pkey INT PRIMARY KEY AUTO_INCREMENT NOT NULL,	
+	id_docente INT, INDEX(id_docente), FOREIGN KEY (id_docente) REFERENCES docente(pkey),
+	id_categoria_incidencia INT, INDEX(id_categoria_incidencia), FOREIGN KEY (id_categoria_incidencia) REFERENCES categoria_incidencia(pkey),
+	id_periodo INT, INDEX(id_periodo), FOREIGN KEY (id_periodo) REFERENCES periodo(pkey),
+	id_programa_educativo INT, INDEX(id_programa_educativo), FOREIGN KEY (id_programa_educativo) REFERENCES programa_educativo(pkey),
+	descripcion TEXT NOT NULL,
+	fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	fecha_ultima_mod TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO categoria_incidencia(nombre) VALUES("Incidencia 1");
+INSERT INTO categoria_incidencia(nombre) VALUES("Incidencia 2");
+INSERT INTO categoria_incidencia(nombre) VALUES("Incidencia 3");
+INSERT INTO categoria_incidencia(nombre) VALUES("Incidencia 4");
+INSERT INTO categoria_incidencia(nombre) VALUES("Incidencia 5");
+
+INSERT INTO periodo(nombre, clave) VALUES("Enero-Abril", "Ene-Abr");
+INSERT INTO periodo(nombre, clave) VALUES("Mayo-Agosto", "May-Ago");
+INSERT INTO periodo(nombre, clave) VALUES("Septiembre-Diciembre", "Sep-Dic");
+
+INSERT INTO criterio(nombre, clave) VALUES("Criterio 1", "CR01");
+INSERT INTO criterio(nombre, clave) VALUES("Criterio 2", "CR02");
+INSERT INTO criterio(nombre, clave) VALUES("Criterio 3", "CR03");
+INSERT INTO criterio(nombre, clave) VALUES("Criterio 4", "CR04");
+INSERT INTO criterio(nombre, clave) VALUES("Criterio 5", "CR05");
+
+INSERT INTO programa_educativo(nombre, clave) VALUES("Ingenieria en Software", "ING_SOFT");
+INSERT INTO programa_educativo(nombre, clave) VALUES("Ingenieria en Telematica", "ING_TELM");
+
+INSERT INTO docente(
+	nombre,
+	apellido_paterno,
+	apellido_materno,
+	correo_electronico,
+	fecha_nacimiento,
+	cedula_profesional,
+	numero_telefonico,
+	fotografia,
+	tipo_horario) 
+VALUES(
+	"Luis Fernando", 
+	"Hernández",
+	"Méndez",
+	"lfernando2091@gmail.com",
+	"1998-06-23",
+	"12345ABCDE",
+	"7711877577",
+	"c:\foto.jpg",
+	"parcial"
+	);
+INSERT INTO docente(
+	nombre,
+	apellido_paterno,
+	apellido_materno,
+	correo_electronico,
+	fecha_nacimiento,
+	cedula_profesional,
+	numero_telefonico,
+	fotografia,
+	tipo_horario) 
+VALUES(
+	"Profesor 1", 
+	"Paterno 1",
+	"Materno 1",
+	"correo1@gmail.com",
+	"1998-06-23",
+	"12345ABCDE",
+	"7711877577",
+	"c:\foto1.jpg",
+	"completo"
+	);
+INSERT INTO docente(
+	nombre,
+	apellido_paterno,
+	apellido_materno,
+	correo_electronico,
+	fecha_nacimiento,
+	cedula_profesional,
+	numero_telefonico,
+	fotografia,
+	tipo_horario) 
+VALUES(
+	"Profesor 2", 
+	"Paterno 2",
+	"Materno 2",
+	"correo2@gmail.com",
+	"1998-06-23",
+	"12345ABCDE",
+	"7711877577",
+	"c:\foto2.jpg",
+	"completo"
+	);
+
+INSERT INTO docente_bajas(id_docente, comentario, tipo_baja) VALUES(2, "Tiene otro empleo", "definitivo");
