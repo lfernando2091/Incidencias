da7f0306bc3d5f0a7d409dde9c8ea36ad25266c9
lfernando2091 <lfernando2091@gmail.com>
2018-12-21 15:58:12 -0600

    Periodo
    Agregar Periodo

--- routes/menu/periodo.js
+++ routes/menu/periodo.js
@@ -5,7 +5,7 @@ var router = express.Router();
 const mappers = require('../../lib/mappers.js');
 mappers.loadMapper('/menu/periodo');
 
-/* GET home page. */
+/* GET Periodo. */
 router.get('/', function(req, res, next){
 	req.getConnection(function(err, connection) {
 	      if (err) return next(err);
@@ -18,10 +18,26 @@ router.get('/', function(req, res, next){
 			return res.redirect('/welcome'); 
 
 	      });
-	      
 	});
+});
 
-	
+/* GET Periodo.*/
+router.get('/agregar', function(req, res, next){
+	req.getConnection(function(err, connection) {
+	      if (err) return next(err);
+			var param = { 
+		        nombre : req.body.username,
+		        clave : req.body.email
+		    } 
+	      connection.query(mappers.onQuery('periodo', 'agregarPeriodo', param), [], function(err, results) {
+	        if (err) return next(err); 
+	        
+			req.flash('results', results);
+	        req.flash('content', 'periodo');
+			return res.redirect('/welcome'); 
+
+	      });
+	});
 });
 
 module.exports = router;
